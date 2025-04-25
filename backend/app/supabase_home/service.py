08995ac4b9f4034e25e493e55de3091adb58bc1a
TechWithTy <tyrique1.daniel@gmail.com>
2025-04-24 19:16:49 -0600

    removed go high level

--- backend/app/supabase_home/service.py
+++
@@ -1,9 +0,0 @@
-# Re-export SupabaseService from _service.py for compatibility with tests
-from ._service import (
-    SupabaseAPIError,
-    SupabaseAuthError,
-    SupabaseError,
-    SupabaseService,
-)
-
-__all__ = ['SupabaseService', 'SupabaseError', 'SupabaseAuthError', 'SupabaseAPIError']
